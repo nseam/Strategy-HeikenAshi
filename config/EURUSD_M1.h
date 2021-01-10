5a6b9b9c0b5102892aa8df9d3cd5e30c7fbc9313
kenorb <kenorb@users.noreply.github.com>
2021-01-10 17:30:14 +0000

    Optimize Indicator params (pf=0.41; dd=13.23; tt=336)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,7 +1,17 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
+
+// Defines indicator's parameter values for the given pair symbol and timeframe.
+struct Indi_HeikenAshi_Params_M1 : Indi_HeikenAshi_Params {
+  Indi_HeikenAshi_Params_M1() : Indi_HeikenAshi_Params(indi_ha_defaults, PERIOD_M1) {
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_method = 0;
+    period = 0;
+    shift = 0;
+  }
+} indi_ha_m1;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
 struct Stg_HeikenAshi_Params_M1 : StgParams {
@@ -10,12 +20,12 @@ struct Stg_HeikenAshi_Params_M1 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
