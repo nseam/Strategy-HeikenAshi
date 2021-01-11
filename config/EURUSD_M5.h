6a5673c5b08633fabddf1d8f79474e5ff8b04b87
kenorb <kenorb@users.noreply.github.com>
2021-01-11 04:57:32 +0000

    Optimize SignalOpen params (pf=3.81; dd=0.20; tt=20)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -1,21 +1,31 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
+
+// Defines indicator's parameter values for the given pair symbol and timeframe.
+struct Indi_HeikenAshi_Params_M5 : Indi_HeikenAshi_Params {
+  Indi_HeikenAshi_Params_M5() : Indi_HeikenAshi_Params(indi_ha_defaults, PERIOD_M5) {
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_method = 0;
+    period = 0;
+    shift = 0;
+  }
+} indi_ha_m5;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
 struct Stg_HeikenAshi_Params_M5 : StgParams {
   // Struct constructor.
   Stg_HeikenAshi_Params_M5() : StgParams(stg_ha_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)0;
-    signal_open_boost = 0;
+    signal_open_method = -4;
+    signal_open_filter = 26;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
